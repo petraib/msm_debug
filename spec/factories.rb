97b59d3f11886091c926a6b806fe74b327708ee4
Petra Bakosova <petrab@chicagobooth.edu>
2017-07-26 02:38:11 +0000

    try now

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,19 +1,25 @@
 # spec/factories/articles.rb
+ require Rails.root.join("spec", "support", "increasing_random.rb")
+
 FactoryGirl.define do
+  factory :director do
+    dob "January 1, 1970"
+    name "Someone"
+    bio "Someone's background"
+    image_url "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg"
+    #sequence(:id, IncreasingRandom.new) { |n| n.value }
+    #director_id = 1
+  end
+  
   factory :movie do
     title "The Godfather: Part VI"
     year 2016
     duration 180
     description "Not a real movie"
     image_url "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg"
+    #sequence(:director_id, IncreasingRandom.new) { |n| "#{n}" }
+    #association :director, factory: :director
     director
-  end
-
-  factory :director do
-    dob "January 1, 1970"
-    name "Someone"
-    bio "Someone's background"
-    image_url "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg"
   end
 
   factory :actor do
@@ -21,6 +27,7 @@ FactoryGirl.define do
     name "Someone"
     bio "Someone's background"
     image_url "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg"
+    #sequence(:id, IncreasingRandom.new) { |n| "#{n}" }
   end
 
   factory :role do
@@ -28,4 +35,18 @@ FactoryGirl.define do
     movie
     actor
   end
+  
+  sequence(:email) { |n| "person-#{n}@example.com" }
+
+  factory :user do
+    email
+  end
+
+  factory :post do
+    user
+    title "Hello"
+  end
+
+
+
 end
